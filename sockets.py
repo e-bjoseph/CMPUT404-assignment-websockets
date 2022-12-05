b2ec3748c52b2d6c9ebcb27224e00004f07f5325
Ervin Binu Joseph <39553090+e-bjoseph@users.noreply.github.com>
2022-12-05 03:48:49 -0700

    update

--- sockets.py
+++ sockets.py
@@ -83,7 +83,7 @@ myWorld.add_set_listener( set_listener )
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return flask.redirect("/static/chat.html")
+    return flask.redirect("/static/index.html")
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
